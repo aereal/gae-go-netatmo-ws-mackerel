552b0dc952e36cc5651b1c90948bbd21e69c8f57
aereal <aereal@aereal.org>
2017-12-10 01:16:24 +0900

    Collects data from Netatmo

--- main.go
+++ main.go
@@ -1,14 +1,114 @@
-package main
+package netatmo2mackerel
 
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"reflect"
+	"time"
+
+	netatmo "github.com/aereal/netatmo-api-go"
+	mkr "github.com/mackerelio/mackerel-client-go"
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 )
 
+const (
+	baseURL = "https://api.netatmo.net/"
+	authURL = baseURL + "oauth2/token"
+)
+
+var (
+	netatmoEmail     = os.Getenv("NETATMO_EMAIL")
+	netatmoPassword  = os.Getenv("NETATMO_PASSWORD")
+	netatmoAppID     = os.Getenv("NETATMO_APP_ID")
+	netatmoAppSecret = os.Getenv("NETATMO_APP_SECRET")
+)
+
+var metricPrefixes = map[string]string{
+	"BatteryPercent": "battery",
+	"RFStatus":       "rf_status",
+	"WifiStatus":     "wifi",
+	"Temperature":    "temperature",
+	"Humidity":       "humidity",
+	"CO2":            "co2",
+	"Noise":          "noise",
+	"Pressure":       "pressure",
+}
+
 func init() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/metrics", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hi")
+	ctx := appengine.NewContext(r)
+	metricsValues, err := fetchWeatherStationMetrics(ctx)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Failed: (%s) %#v", err, err)
+	}
+	fmt.Fprintf(w, "%#v", metricsValues)
+}
+
+func fetchWeatherStationMetrics(ctx context.Context) ([]*mkr.MetricValue, error) {
+	netatmoClient, err := netatmo.NewClientWithContext(ctx, netatmo.Config{
+		ClientID:     netatmoAppID,
+		ClientSecret: netatmoAppSecret,
+		Username:     netatmoEmail,
+		Password:     netatmoPassword,
+	})
+	dc, err := netatmoClient.Read()
+	if err != nil {
+		return nil, err
+	}
+	metrics := make([]*mkr.MetricValue, 0)
+	for _, station := range dc.Stations() {
+		for _, module := range station.Modules() {
+			infoEpoch, info := module.Info()
+			infoTs := time.Unix(infoEpoch, 0)
+			for name, value := range info {
+				log.Debugf(ctx, "Timestamp:%s Station:%s Module:%s Name:%s Value:%#v\n", infoTs, module.StationName, module.ModuleName, name, value)
+				if metricPrefix, ok := metricPrefixes[name]; ok {
+					if v, ok := float64of(value); ok {
+						metrics = append(metrics, &mkr.MetricValue{
+							Name:  metricPrefix + "." + module.ModuleName,
+							Value: v,
+							Time:  infoTs.Unix(),
+						})
+					}
+				}
+			}
+
+			dataEpoch, data := module.Data()
+			dataTs := time.Unix(dataEpoch, 0)
+			for name, value := range data {
+				log.Debugf(ctx, "Timestamp:%s Station:%s Module:%s Name:%s Value:%#v\n", dataTs, module.StationName, module.ModuleName, name, value)
+				if metricPrefix, ok := metricPrefixes[name]; ok {
+					if v, ok := float64of(value); ok {
+						metrics = append(metrics, &mkr.MetricValue{
+							Name:  metricPrefix + "." + module.ModuleName,
+							Value: v,
+							Time:  dataTs.Unix(),
+						})
+					}
+				}
+			}
+		}
+	}
+	return metrics, nil
+}
+
+func float64of(value interface{}) (float64, bool) {
+	if v, ok := value.(reflect.Value); ok {
+		switch v.Kind() {
+		case reflect.Float32, reflect.Float64:
+			return v.Float(), true
+		case reflect.Int32, reflect.Int64:
+			return float64(v.Int()), true
+		default:
+			return 0, false
+		}
+	}
+	return 0, false
 }
